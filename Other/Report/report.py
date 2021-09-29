7f6148352ab2fedca40bc62187a7a53f5cb809c5
MikhailParkin <parkfes@bk.ru>
2021-09-29 11:55:10 +0300

    fix report

--- Other/Report/report.py
+++ Other/Report/report.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore
 import report_gui
 import yaml
 import mysql.connector
+import xlsxwriter
 
 
 class ParentWindow(QMainWindow, report_gui.Ui_MainWindow):
@@ -19,6 +20,7 @@ class ParentWindow(QMainWindow, report_gui.Ui_MainWindow):
     month_end = None
     year_start = None
     year_end = None
+    calls = None
 
     def __init__(self):
         super().__init__()
@@ -30,6 +32,8 @@ class ParentWindow(QMainWindow, report_gui.Ui_MainWindow):
         self.dateEdit.dateChanged.connect(self.date_change)
         self.dateEdit_2.dateChanged.connect(self.date_change)
         self.pushButton.clicked.connect(self.load_report)
+        self.pushButton_2.clicked.connect(lambda: self.save_xlsx(calls=self.calls))
+        self.dlg_m = QMessageBox()
 
     def date_change(self):
         date_start = self.dateEdit.date()
@@ -41,8 +45,15 @@ class ParentWindow(QMainWindow, report_gui.Ui_MainWindow):
         self.month_end = date_end.month()
         self.year_end = date_end.year()
 
+    def alert_message(self, text, message):
+        self.dlg_m.show()
+        self.dlg_m.setIcon(QMessageBox.Information)
+        self.dlg_m.setWindowTitle("Info")
+        self.dlg_m.setText(text)
+        self.dlg_m.setInformativeText(message)
+
     def load_settings(self):
-        with open('settings.yaml', 'r') as f:
+        with open('Report/settings.yaml', 'r') as f:
             config = yaml.safe_load(f)
         self.username_sql = config.get('username')
         self.password_sql = config.get('password')
@@ -89,6 +100,44 @@ class ParentWindow(QMainWindow, report_gui.Ui_MainWindow):
                 table_item.append(item)
             model.insertRow(row_number, table_item)
         self.tableView.setModel(model)
+        self.calls = calls
+
+    def save_xlsx(self, calls):
+
+        # Create an new Excel file and add a worksheet.
+        try:
+            workbook = xlsxwriter.Workbook(f'Отчёт{self.year_start}{self.month_start}{self.day_end}.xlsx')
+        except FileExistsError as e:
+            self.alert_message('Ошибка', 'Файл не создан')
+        worksheet = workbook.add_worksheet()
+
+        # Widen the first column to make the text clearer.
+        worksheet.set_column('A:A', 15)
+        worksheet.set_column('B:B', 18)
+        worksheet.set_column('C:C', 10)
+        worksheet.set_column('D:D', 18)
+        worksheet.set_column('E:E', 18)
+        worksheet.set_column('F:F', 18)
+
+        # Add a bold format to use to highlight cells.
+        # bold = workbook.add_format({'bold': True})
+        # header = workbook.add_format()
+
+        # Write some simple text. 'Лицевой', 'Номер телефона', 'Попыток', 'Успешных дозвонов', 'Время разговора', 'Сумма задолжности'
+        worksheet.write('A1', 'Лицевой')
+        worksheet.write('B1', 'Номер телефона')
+        worksheet.write('C1', 'Попыток')
+        worksheet.write('D1', 'Успешных дозвонов')
+        worksheet.write('E1', 'Время разговора')
+        worksheet.write('F1', 'Сумма задолжности')
+
+        # Write some numbers, with row/column notation.
+        for row_number, call in enumerate(calls):
+            for value, values in enumerate(call):
+                worksheet.write(row_number + 1, value, values)
+
+        workbook.close()
+        self.alert_message('', f'Файл создан, сохранено {len(calls)} записей')
 
 
 if __name__ == '__main__':
