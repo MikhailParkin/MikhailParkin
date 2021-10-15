5edc5ff02bc70ff09f00818aeccc4c14be7afbfc
MikhailParkin <parkfes@bk.ru>
2021-10-15 13:50:08 +0300

    add homework_03

---
+++ homework_03/my_app.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello!!!"}
+
+
+@app.get("/ping/")
+def pong():
+    return {"message": "pong"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
